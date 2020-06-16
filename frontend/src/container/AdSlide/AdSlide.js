d09f3b41ada0e93b9229d29285ef7a185d33ac47
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-16 17:55:08 -0500

    Se crea los logos de accout en la pagina principal

--- frontend/src/container/AdSlide/AdSlide.js
+++ frontend/src/container/AdSlide/AdSlide.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AdCarousel from '../../components/AdCarousel/AdCarousel';
+import AccountUser from '../AccountUser/AccountUser';
 
 const AdSlide = () => {
   return (
     <React.Fragment>
       <AdCarousel/>
+      <br/>
+      <AccountUser/>
     </React.Fragment>
   )
 }
