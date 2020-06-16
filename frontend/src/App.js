af5ec334255ac6c5f0311098135981bea6c899dc
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-16 17:06:19 -0500

    Se agregan vista para visualizar su forma

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navbar from './container/Navbar/Navbar';
+import AdSlide from './container/AdSlide/AdSlide';
+import Login from './container/Login/Login';
+import NewUser from './container/NewUser/NewUser';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar/>
+      <AdSlide/>
+      <Login/>
+      <NewUser/>
     </React.Fragment>
   );
 }
