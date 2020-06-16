a902ec94e997ece1dabded9d3b0917a1c2aee65b
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-16 17:04:45 -0500

    Se crea y agrega Slide para los anuncions

---
+++ frontend/src/container/AdSlide/AdSlide.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdCarousel from '../../components/AdCarousel/AdCarousel';
+
+const AdSlide = () => {
+  return (
+    <React.Fragment>
+      <AdCarousel/>
+    </React.Fragment>
+  )
+}
+
+export default AdSlide;
