646c0f7dca15c841fd8732b80fa06063a48ad0e4
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-16 17:05:35 -0500

    Se agrega y crea Login

---
+++ frontend/src/container/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginUser from '../../components/LoginUser/LoginUser'
+
+const Login = () => {
+  return (
+    <React.Fragment>
+      <LoginUser />
+    </React.Fragment>
+  )
+}
+
+export default Login
