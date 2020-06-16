bea460450b1c4b3d2042cee5a4df366e31c9ff11
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-16 17:56:12 -0500

    Se utiliza reac-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter,Route, Switch} from 'react-router-dom';
 import Navbar from './container/Navbar/Navbar';
 import AdSlide from './container/AdSlide/AdSlide';
 import Login from './container/Login/Login';
@@ -7,12 +8,19 @@ import NewUser from './container/NewUser/NewUser';
 function App() {
   return (
     <React.Fragment>
+      <BrowserRouter>
       <Navbar/>
-      <AdSlide/>
-      <Login/>
-      <NewUser/>
+      <Switch>
+        <Route exact path="/" component={AdSlide}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/newuser" component={NewUser}/>
+      </Switch>
+      </BrowserRouter>
     </React.Fragment>
   );
 }
 
 export default App;
+/*
+<Route component={NotFound}/>
+*/
