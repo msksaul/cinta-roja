73485a5c66e55e421bc3291a95e415af4815cf32
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-19 16:01:22 -0500

    ultima configuracion

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,7 +2,7 @@ const { NODE_ENV } = process.env;
 
 const config = {
   production: {
-    MONGO_URI: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cintanegra-shard-00-00-wygjh.mongodb.net:27017,cintanegra-shard-00-01-wygjh.mongodb.net:27017,cintanegra-shard-00-02-wygjh.mongodb.net:27017/prueba?ssl=true&replicaSet=CintaNegra-shard-0&authSource=admin&retryWrites=true&w=majority'`,
+    MONGO_URI: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cintan-shard-00-00.wygjh.mongodb.net:27017,cintan-shard-00-01.wygjh.mongodb.net:27017,cintan-shard-00-02.wygjh.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-p0i91b-shard-0&authSource=admin&retryWrites=true&w=majority`,
   },
   staging: {
     MONGO_URI: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cintan-shard-00-00.wygjh.mongodb.net:27017,cintan-shard-00-01.wygjh.mongodb.net:27017,cintan-shard-00-02.wygjh.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-p0i91b-shard-0&authSource=admin&retryWrites=true&w=majority`,
